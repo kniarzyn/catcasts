d82949255e151355093bb50ea326357187736fc7
Paweł Kwietniewski <kniarzyn@gmail.com>
2020-02-18 12:29:02 +0100

    Add navbar template

--- test/catcasts_web/templates/layout/navigation_test.exs
+++ test/catcasts_web/templates/layout/navigation_test.exs
@@ -4,6 +4,6 @@ defmodule CatcastsWeb.NavigationTest do
   test "shows a sign in with Google link when not signed in", %{conn: conn} do
     conn = get(conn, "/")
 
-    assert html_response(conn, 200) =~ "sign in with Google"
+    assert html_response(conn, 200) =~ "Sign in with Google"
   end
 end

58095bbdfa20a4352cb1a6d5f9440e55523f9e35
Paweł Kwietniewski <kniarzyn@gmail.com>
2020-02-18 11:55:52 +0100

    Remove stuff from templates

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -103,9 +103,9 @@ eval("\n\n(function () {\n  var PolyfillEvent = eventConstructor();\n\n  functio
   !*** ./css/app.css ***!
   \*********************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\\nError: No PostCSS Config found in: /home/kniarzyn/projects/elixir/catcasts/assets/css\\n    at config.search.then (/home/kniarzyn/projects/elixir/catcasts/assets/node_modules/postcss-load-config/src/index.js:91:15)\\n    at runLoaders (/home/kniarzyn/projects/elixir/catcasts/assets/node_modules/webpack/lib/NormalModule.js:316:20)\\n    at /home/kniarzyn/projects/elixir/catcasts/assets/node_modules/loader-runner/lib/LoaderRunner.js:367:11\\n    at /home/kniarzyn/projects/elixir/catcasts/assets/node_modules/loader-runner/lib/LoaderRunner.js:233:18\\n    at context.callback (/home/kniarzyn/projects/elixir/catcasts/assets/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\\n    at Promise.resolve.then.then.catch (/home/kniarzyn/projects/elixir/catcasts/assets/node_modules/postcss-loader/src/index.js:208:9)\");\n\n//# sourceURL=webpack:///./css/app.css?");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./css/app.css?");
 
 /***/ }),
 

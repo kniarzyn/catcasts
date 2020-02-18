938538695ddc31f3914862a5aa575edf9c49d1d6
Paweł Kwietniewski <kniarzyn@gmail.com>
2020-02-18 13:02:28 +0100

    Add style&logic to allerts

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,9 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+window.setTimeout(function() {
+  let alerts = document.getElementsByClassName("alert");
+  alerts[0].style.display = 'none';
+  alerts[1].style.display = 'none';
+}, 3000)
